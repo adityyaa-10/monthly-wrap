290a66f29c8a16f1b1f648cd549d360e7f469792
Aditya <aditya.pandey.241103@gmail.com>
2023-06-05 13:57:50 +0530

    api calling for blog create using axios

--- backend/settings.py
+++ backend/settings.py
@@ -55,15 +55,17 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
-
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+]
 ROOT_URLCONF = 'backend.urls'
 
 TEMPLATES = [
@@ -202,6 +204,22 @@ SIMPLE_JWT = {
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
 
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
 LOGIN_REDIRECT_URL = 'home'
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+]
+
 PASSWORD_RESET_TIMEOUT = 600
--- frontend/src/components/BlogCreate.jsx
+++ frontend/src/components/BlogCreate.jsx
@@ -1,6 +1,66 @@
+import { useState, useCallback, useEffect, useMemo } from "react";
 import defaultblogpic from '../assets/Images/selectcover.png'
-import BlogRTF from './BlogRTF'
+// import BlogRTF from './BlogRTF'
+import axios from 'axios'
+import JoditEditor from "jodit-react";
 const BlogCreate = () => {
+    const [title, setTitle] = useState('');
+    const [category, setCategory] = useState('');
+    const [content, setContent] = useState("");
+    const [logs, setLogs] = useState([]);
+    const appendLog = useCallback(
+        (message) => {
+            console.log("logs = ", logs);
+            const newLogs = [...logs, message];
+            setLogs(newLogs);
+        },
+        [logs, setLogs]
+    );
+
+    const config = useMemo(
+        () => ({
+            readonly: false
+        }),
+        []
+    );
+
+    const onChange = useCallback(
+        (newContent) => {
+            appendLog(`onChange triggered with ${newContent}`);
+        },
+        [appendLog]
+    );
+
+    useEffect(() => {
+        console.log("onChange = ", onChange);
+    }, [onChange]);
+
+    const onBlur = useCallback(
+        (newContent) => {
+            appendLog(`onBlur triggered with ${newContent}`);
+            setContent(newContent);
+        },
+        [appendLog, setContent]
+    );
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+
+        const blogData = {
+            title: title,
+            category: category,
+            content: content,
+        };
+
+        axios.post('http://127.0.0.1:8000/create/', blogData)
+            .then(response => {
+                // Handle successful response
+                console.log(response.data);
+            })
+            .catch(error => {
+                // Handle error response
+                console.error(error);
+            });
+    };
     return (
         <div>
             <section className="body-font relative max-w-screen-lg mx-auto">
@@ -9,25 +69,31 @@ const BlogCreate = () => {
                         <h1 className="xl:text-3xl sm:text-2xl text-xl font-semibold mb-4 "> <span className='text-blue'>Create</span> and <span className='text-blue'>Customise</span> your blog here!</h1>
                         <img src={defaultblogpic} alt='default' className=" md:h-84 w-full mx-auto my-2 object-center object-fill rounded-lg shadow-md" />
                     </div>
-                    <div className="mx-auto">
+                    <form onSubmit={handleFormSubmit} className="mx-auto">
                         <div className="flex flex-wrap -m-2">
-                            <div className='p-2 w-full right-0'>
+                            {/* <div className='p-2 w-full right-0'>
                                 <h2 className='pb-5'>Select a cover image for your blog</h2>
-                                <input type='file' accept='image/*' alt='img' />
-                            </div>
+                                <input type='file' accept='image/*' alt='img' onChange={(e) => console.log(e.target.value)} />
+                            </div> */}
                             <div className="p-2 w-full">
                                 <div className="relative">
-                                    <input type="text" placeholder='Title of your blog' id="title" name="title" className="w-full bg-gray-200 rounded border text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                                    <input value={title} onChange={e => setTitle(e.target.value)} type="text" placeholder='Title of your blog' id="title" name="title" className="w-full bg-gray-200 rounded border text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                                 </div>
                             </div>
                             <div className="p-2 w-full">
                                 <div className="relative">
-                                    <input type="text" placeholder='Category of Blog' id="blogCategory" name="blogCategory" className="w-full bg-gray-200 rounded border text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                                    <input type="text" value={category} onChange={e => setCategory(e.target.value)} placeholder='Category of Blog' id="blogCategory" name="blogCategory" className="w-full bg-gray-200 rounded border text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                                 </div>
                             </div>
                             <div className="p-2 w-full">
-                                <div className="relative">
-                                    <BlogRTF />
+                                <div className="relative text-primary">
+                                    <JoditEditor
+                                        value={content}
+                                        config={config}
+                                        tabIndex={1}
+                                        onBlur={onBlur}
+                                        onChange={onChange}
+                                    />
                                 </div>
                             </div>
                             <div className="p-2 w-full">
@@ -35,7 +101,7 @@ const BlogCreate = () => {
                             </div>
                             <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center" />
                         </div>
-                    </div>
+                    </form>
                 </div>
             </section>
         </div>
