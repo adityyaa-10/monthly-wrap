f21f079b1486374bbd49de2fc843e34425319743
Devyanshi Srivastava <devyanshi2106@gmail.com>
2023-06-05 21:57:11 +0530

    fix:minor errors

--- magazine/urls.py
+++ magazine/urls.py
@@ -5,7 +5,7 @@ from .views import UserPostAPIView, LikesAPIView, CommentAPIView, BlogDetailAPIV
 
 urlpatterns = [
     path('', BlogListAPIView.as_view(), name='home'),
-    path('http://127.0.0.1:8000/create',BlogCreateAPIView.as_view(), name='create'),
+    path('create/',BlogCreateAPIView.as_view(), name='create'),
     path('<slug:slug>/', BlogDetailAPIView.as_view()),
     path('users/<str:username>/', UserPostAPIView.as_view()),
     path('<slug:slug>/likes/', LikesAPIView.as_view()),
