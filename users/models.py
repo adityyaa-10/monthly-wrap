ee34a700311e17b417d7a289a7761a6c6606544d
Devyanshi Srivastava <devyanshi2106@gmail.com>
2023-06-11 17:27:58 +0530

    fix:Profile section

--- users/models.py
+++ users/models.py
@@ -4,25 +4,30 @@ from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    about = models.TextField()
-    phone_no = models.CharField(max_length=200)
-    kiet_email = models.EmailField()
-    github_link = models.URLField()
-    twitter_link = models.URLField()
-    linkedin_link = models.URLField()
-    techstack = models.TextField()
-    profile_picture = models.ImageField()
-    other_interests = models.TextField()
+    name = models.CharField(max_length=255, default='')
+    about = models.TextField(blank=True)
+    phone_no = models.CharField(max_length=200, blank=True)
+    email = models.EmailField(default = '')
+    github_link = models.URLField(blank=True)
+    twitter_link = models.URLField(blank=True)
+    linkedin_link = models.URLField(blank=True)
+    techstack = models.TextField(blank=True)
+    other_interests = models.TextField(blank=True)
 
     def __str__(self):
         return self.user.username
     
     def save(self, *args, **kwargs):
+        if not self.email:
+            self.email = self.user.email
+        if not self.name:
+            self.name = f"{self.user.first_name} {self.user.last_name}"
         super().save(*args, **kwargs)
 
-        img = Image.open(self.profile_picture.path)
 
-        if img.height > 400 or img.width > 400:
-            output_img = (400,400)
-            img.thumbnail(output_img)           
-            img.save(self.profile_picture.path)
+        # img = Image.open(self.profile_picture.path)
+
+        # if img.height > 400 or img.width > 400:
+        #     output_img = (400,400)
+        #     img.thumbnail(output_img)           
+        #     img.save(self.profile_picture.path)
