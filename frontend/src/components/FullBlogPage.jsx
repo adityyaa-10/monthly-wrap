fa157f9d73fb5b84efe7af3a182aafbd887f6b3d
Aditya <aditya.pandey.241103@gmail.com>
2023-06-23 21:23:04 +0530

    Likes and Comments api completed

--- frontend/src/components/FullBlogPage.jsx
+++ frontend/src/components/FullBlogPage.jsx
@@ -2,17 +2,17 @@ import { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import reactbg from '../assets/Images/react.png';
 import Cookies from 'js-cookie';
-import leftarrow from '../assets/Images/leftarrow.png'
+import { ArrowLeft } from 'feather-icons-react';
+import Sidebar from './Sidebar';
 const FullBlogPage = () => {
     const { title } = useParams();
     const navigate = useNavigate();
     const [author, setAuthor] = useState('');
-    const [category, setCategory] = useState('');
     const [fullBlogContent, setFullBlogContent] = useState('');
-    // const [content, setContent] = useState('');
     const [newContent, setNewContent] = useState('');
     const [likes, setLikes] = useState(0);
-    // Fetch the full blog content, comments, and likes from your API or data source
+    const [comments, setComments] = useState([]);
+
     useEffect(() => {
         const fetchBlogDetail = async () => {
             try {
@@ -34,8 +34,9 @@ const FullBlogPage = () => {
                 } else {
                     const data = await response.json();
                     setAuthor(data.user);
-                    setCategory(data.category);
                     setFullBlogContent(data.content);
+                    setLikes(data.likes_count);
+                    setComments(data.comments);
                 }
             } catch (error) {
                 console.error(error);
@@ -63,8 +64,9 @@ const FullBlogPage = () => {
 
             const data = await response.json();
             setAuthor(data.author);
-            setCategory(data.category);
             setFullBlogContent(data.content);
+            setLikes(data.likes_count);
+            setComments(data.comments);
             Cookies.set('new_access_token', data.access_token);
         } catch (error) {
             console.error(error);
@@ -97,72 +99,96 @@ const FullBlogPage = () => {
         }
     };
 
-    // const handleComment = async (e) => {
-    //     e.preventDefault();
+    const handleComment = async (e) => {
+        e.preventDefault();
 
-    //     try {
-    //         const new_access_token = Cookies.get('new_access_token');
+        try {
+            const new_access_token = Cookies.get('new_access_token');
 
-    //         const response = await fetch(`http://127.0.0.1:8000/${title}/comment/`, {
-    //             method: 'POST',
-    //             headers: {
-    //                 'Content-Type': 'application/json',
-    //                 Authorization: `Bearer ${new_access_token}`,
-    //             },
-    //             body: JSON.stringify({ content: newContent }),
-    //         });
+            const response = await fetch(`http://127.0.0.1:8000/${title}/comment/`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${new_access_token}`,
+                },
+                body: JSON.stringify({ content: newContent }),
+            });
 
-    //         if (response.status === 401) {
-    //             await refreshAccessToken();
-    //             handleComment(e); // Retry commenting after token refresh
-    //         } else if (!response.ok) {
-    //             throw new Error('Failed to add comment');
-    //         } else {
-    //             const data = await response.json();
-    //             setContent(data.content);
-    //             setNewContent('');
-    //         }
-    //     } catch (error) {
-    //         console.error(error);
-    //     }
-    // };
+            if (response.status === 401) {
+                await refreshAccessToken();
+                handleComment(e); // Retry commenting after token refresh
+            } else if (!response.ok) {
+                throw new Error('Failed to add comment');
+            } else {
+                const data = await response.json();
+                setComments([...comments, data]);
+                setNewContent(''); // Clear the comment input
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
     const goBack = () => {
         navigate(-1);
     };
 
     return (
-        <div className="container mx-auto py-24 px-4">
-            <div className="max-w-2xl mx-auto bg-primary rounded-lg shadow-lg p-6">
-                <div
-                    onClick={goBack}
-                    className=" text-white font-semibold py-2 px-4 rounded mb-3"
-                >
-                    <img src={leftarrow} alt="" className='w-11 h-11' />
-                </div>
-                <img src={reactbg} alt="Blog" className="mb-4 rounded-lg" />
-                <h1 className="text-3xl text-white font-bold mb-4">{title.toUpperCase()}</h1>
-                <h2 className="text-xl text-dimWhite font-medium mb-4">Category: {category}</h2>
-                <h3 className="text-lg text-white mb-4">Author: <span>{author}</span></h3>
-                <p className="text-base leading-7 mb-8 text-white" dangerouslySetInnerHTML={{ __html: fullBlogContent }}></p>
-                <div className="flex items-center mb-4">
-                    <button
-                        onClick={handleLike}
-                        className="bg-indigo-500 hover:bg-indigo-700 text-white font-semibold py-2 px-4 rounded mr-3"
-                    >
-                        Like ({likes})
-                    </button>
-                    <textarea
-                        value={newContent}
-                        onChange={(e) => setNewContent(e.target.value)}
-                        className="w-full border border-gray-300 rounded p-2"
-                        placeholder="Add a comment"
-                    ></textarea>
-                    <button
-                        className="bg-indigo-500 hover:bg-indigo-700 text-white font-semibold py-2 px-4 rounded ml-3"
-                    >
-                        Comment
-                    </button>
+        <div>
+            <div className='float-left'>
+                <Sidebar />
+            </div>
+            <div className="p-4 sm:ml-64 ">
+                <div className="p-4 border-2 border-dashed rounded-lg border-gray-700 mt-16 text-white">
+                    <div className="container mx-auto py-12 px-4 ">
+                        <div className="max-w-5xl mx-auto bg-primary rounded-lg shadow-lg p-6">
+                            <button
+                                onClick={goBack}
+                                className="bg-blue text-white font-semibold py-2 px-3 rounded mb-3 flex items-center"
+                                style={{ color: 'white' }} // Set the color to white
+                            >
+                                <ArrowLeft className="mr-2" /> Go Back
+                            </button>
+
+                            <img src={reactbg} alt="Blog" className="mb-4 rounded-lg w-full" />
+                            <h1 className="text-3xl text-white font-bold mb-4">{title.toUpperCase().replace(/-/g, ' ')}</h1>
+                            <h3 className="text-lg text-white mb-4">Published by <span className='text-blue font-semibold'>{author}</span></h3>
+                            <p className="text-base leading-7 mb-8 text-white" dangerouslySetInnerHTML={{ __html: fullBlogContent }}></p>
+                            <div className="flex flex-col mb-7">
+                                <button
+                                    onClick={handleLike}
+                                    className="bg-blue text-white font-semibold py-2 px-4 rounded mb-5 w-28"
+                                >
+                                    Like ({likes})
+                                </button>
+                                <div className="space-y-2">
+                                    {comments.map((comment) => (
+                                        <div key={comment.id} className="bg-gray-100 rounded-lg shadow p-4">
+                                            <p className="text-gray-800 text-base ">{comment.content}</p>
+                                            <p className="text-gray-600 text-sm mt-1">
+                                                By: {comment.user}, {comment.date_posted}
+                                            </p>
+                                        </div>
+                                    ))}
+                                </div>
+                                <div className="flex mt-4">
+                                    <textarea
+                                        value={newContent}
+                                        onChange={(e) => setNewContent(e.target.value)}
+                                        className="flex-grow border border-gray-300 rounded p-2 mr-2 text-gray-800 bg-gray-100"
+                                        placeholder="Add a comment"
+                                    >
+                                    </textarea>
+                                    <button
+                                        onClick={handleComment}
+                                        className="bg-blue text-white font-semibold py-2 px-3 rounded"
+                                    >
+                                        Add Comment
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -170,3 +196,4 @@ const FullBlogPage = () => {
 };
 
 export default FullBlogPage;
+
