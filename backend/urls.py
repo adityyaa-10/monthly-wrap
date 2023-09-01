45c37e8de7968475b99f3c875da727205159fbbb
Devyanshi Srivastava <devyanshi2106@gmail.com>
2023-09-01 14:52:34 +0530

    contact us fixed

--- backend/urls.py
+++ backend/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('magazine.urls')),
     path('api/users/', include('users.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- magazine/views.py
+++ magazine/views.py
@@ -186,5 +186,6 @@ class ContactAPIView(APIView):
         data["user"] = request.user.username
         serializer = ContactSerializer(data=data)
         if serializer.is_valid():
+            serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
